a6cd413f6f1ba1e8e8bb8c970ad14aeb1de04024
Enrique Fleitas <enribarrola@gmail.com>
2024-10-02 00:43:20 -0400

    agrega bootstrap al navbar

--- rrhh-front/src/components/navbar/Navbar.tsx
+++ rrhh-front/src/components/navbar/Navbar.tsx
@@ -1,15 +1,21 @@
 import { Link } from 'react-router-dom';
 
 export default function Navbar() {
-  return (
-    <header className='d-flex justify-content-md-between flex-column flex-md-row align-items-center'>
-      <h1>RR.HH</h1>
+	return (
+		<header className='d-flex justify-content-md-between flex-column flex-md-row align-items-center'>
+			<h1>RR.HH</h1>
 
-      <nav className='d-flex gap-2'>
-        <Link to='/users'>Usuarios</Link>
-        <Link to='/timestamps'>Marcaciones</Link>
-        <Link to='/reports'>Reportes</Link>
-      </nav>
-    </header>
-  );
+			<nav className='d-flex gap-3'>
+				<Link to='/users' className='btn btn-light'>
+					Usuarios
+				</Link>
+				<Link to='/timestamps' className='btn btn-light'>
+					Marcaciones
+				</Link>
+				<Link to='/reports' className='btn btn-light'>
+					Reportes
+				</Link>
+			</nav>
+		</header>
+	);
 }
